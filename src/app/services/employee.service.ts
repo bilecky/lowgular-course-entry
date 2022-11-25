a2834395a5b3bc9a9fc5e572e43369d311dd38f5
bilecky <bilecky95@gmail.com>
2022-11-25 21:37:14 +0100

    Exercise 4 - Delete employee + extra task with alert

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,7 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
+import {map, Observable} from 'rxjs';
+import {PersonModel} from '../model/person.model';
+import {CreateEmployeeModel} from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
@@ -10,5 +11,14 @@ export class EmployeeService {
 
   getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+  }
+
+  create(employee: CreateEmployeeModel): Observable<any> {
+    return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee)
+  }
+
+  delete(id: string): Observable<void> {
+    return this._httpClient.delete(`https://dummy.restapiexample.com/api/v1/delete/${id}`).pipe(map(_ => void 0));
+
   }
 }
