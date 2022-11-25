51c5e73d6d1a0702288e75815b68cddcaeae4e1f
bilecky <bilecky95@gmail.com>
2022-11-25 18:25:26 +0100

    Exercise 3 - Create employee + extra task with alert

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -1,5 +1,7 @@
 import {ChangeDetectionStrategy, Component, ViewEncapsulation} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {EmployeeService} from '../../services/employee.service';
+import {CreateEmployeeModel} from '../../model/create-employee.model';
 
 @Component({
   selector: 'app-employee-form',
@@ -16,8 +18,16 @@ export class EmployeeFormComponent {
 
   });
 
-  onButtonClicked(value: { name: string, age: number, salary: number }) {
-    alert(`User was succesfully added to the database. Email: ${value.name}, Age: ${value.age}, Salary: ${value.salary} $`)
+  constructor(private _employeeService: EmployeeService) {
   }
 
+  // onButtonClicked(value: { name: string, age: number, salary: number }) {
+  //   alert(`User was succesfully added to the database. Email: ${value.name}, Age: ${value.age}, Salary: ${value.salary} $`)
+  // }
+
+  onFormSubmitted(form: CreateEmployeeModel) {
+    this._employeeService.create({name: form.name, age: form.age, salary: form.salary}).subscribe(() => {
+      alert(`User was successfully added to the database. Email: ${form.name}, age: ${form.age}, salary: ${form.salary} `);
+    })
+  }
 }
