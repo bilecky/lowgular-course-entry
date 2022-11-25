85d2dbb37b29c8f3865a14e51eda72226d0b9f52
bilecky <bilecky95@gmail.com>
2022-11-25 23:39:46 +0100

    Exercise 5 - map response to model

---
+++ src/app/services/employee.response.ts
@@ -0,0 +1,12 @@
+export interface EmployeeResponse {
+  status: string;
+
+  id: string;
+  employee_name: string;
+  employee_salary: string;
+  employee_age: string;
+  profile_image: string
+
+}
+
+[]
