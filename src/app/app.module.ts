8ab41e0942d86f91111397674748eaf134c2d11c
bilecky <bilecky95@gmail.com>
2022-11-26 00:04:49 +0100

    FB - Part 2 - Single Page Application - Basic CRUD - project deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,6 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {EmployeeListComponentModule} from "./ui/employee-list/employee-list.component-module";
-import {EmployeeFacesComponentModule} from "./ui/employee-faces/employee-faces.component-module";
-import {EmployeeServiceModule} from "./services/employee.service-module";
-import {ProjectListComponentModule} from "./ui/project-list-component/project-list.component-module";
-import {ProjectServiceModule} from "./services/project.service-module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,11 +10,7 @@ import {ProjectServiceModule} from "./services/project.service-module";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    EmployeeListComponentModule,
-    EmployeeServiceModule,
-    EmployeeFacesComponentModule,
-    ProjectListComponentModule,
-    ProjectServiceModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent],
